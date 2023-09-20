6675a21550247bfbb0fac766dce48660f91e71f9
rlaxodud6842 <rlaxodud6842@naver.com>
2023-09-20 22:50:44 +0900

    10809문제 추가, 정답 참고함, 다시 해볼것

---
+++ BackJoon/String/10809.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        char[] ch = str.toCharArray();
+        int[] answer = new int[26];
+        for(int i=0; i<answer.length; i++) {
+            answer[i] = -1;
+        }
+
+        for(int i=0; i<ch.length; i++) {
+            int n = ch[i] - 97;
+            if(answer[n] == -1) {
+                answer[n] = i;
+            }
+        }
+
+        for(int i=0; i<answer.length; i++) {
+            System.out.print(answer[i]+" ");
+        }
+
+    }
+}
