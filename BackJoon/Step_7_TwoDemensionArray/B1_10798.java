57ab95e78e8c42631f1aaf06796e53f58353b0bc
rlaxodud6842 <rlaxodud6842@naver.com>
2023-10-06 23:52:18 +0900

    10798 세로읽기 문제 추가

---
+++ BackJoon/Step_7_TwoDemensionArray/B1_10798.java
@@ -0,0 +1,32 @@
+import java.io.*;
+
+// https://www.acmicpc.net/problem/10798
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[][] arr = new char[5][15];
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < 5; i++) {
+            String nowToy = br.readLine();
+            for (int j = 0; j < nowToy.length(); j++) {
+                try{
+                    char element = nowToy.charAt(j);
+                    arr[i][j] = element;
+                }catch (ArrayIndexOutOfBoundsException e){
+                    continue;
+                }
+            }
+        }
+
+        for (int i = 0; i < 15; i++) { //찾는 반복문
+            for (int j = 0; j < 5; j++) {
+                String element = String.valueOf(arr[j][i]);
+                if (!String.valueOf(arr[j][i]).isEmpty()){
+                    sb.append(element);}
+            }
+        }
+        System.out.println(sb);
+    }
+}
