1448754375adedbaaeeba422498512df41976ed0
rlaxodud6842 <rlaxodud6842@naver.com>
2024-07-26 20:01:42 +0900

    125681 QueStack 예제는 맞추는 시간초과가 났다.

--- BackJoon/Step_16_StackQueDeck/S3_24511.java
+++ BackJoon/Step_16_StackQueDeck/S3_24511.java
@@ -2,34 +2,53 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
-import java.util.Collection;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 class QueueStack{
-    Object[] myList;
+    Queue<String>[] queueList;
+    Stack<String>[] stackList;
     int N;
     QueueStack(int N){
-        this.myList = new Object[N];
+        this.queueList = new Queue[N];
+        this.stackList = new Stack[N];
         this.N = N;
     }
-    void initialSettingst(String str){
-        for (int i = 0; i < this.N; i++){
-            if(this.myList[i].getClass());
+
+    String inputData(String str, int M){
+        StringBuilder sb = new StringBuilder();
+        //처음 인덱스에서 큐 해보고 아니면 스택으로 간다.
+        StringTokenizer st = new StringTokenizer(str);
+        for (int i = 0; i < M; i++){
+            String number = st.nextToken();
+            for(int j = 0; j < N; j++){
+                try{
+                    this.queueList[j].add(number);
+                    number = this.queueList[j].poll();
+                }catch(Exception e){
+                    this.stackList[j].add(number);
+                    number = this.stackList[j].pop();
+                }
+            }
+            sb.append(number + " ");
         }
+        return sb.toString();
     }
 
-    void dataStructSettings(String str){
+    void dataStructSettings(String str,String initData){
         StringTokenizer st = new StringTokenizer(str);
+        StringTokenizer initst = new StringTokenizer(initData);
         for (int i = 0; i < this.N; i++){
             String dataStructType = st.nextToken();
             if(dataStructType.equals("0")){
                 Queue<String> myQueue = new LinkedList<>();
-                this.myList[i] = myQueue;
+                myQueue.add(initst.nextToken());
+                this.queueList[i] = myQueue;
             }else if (dataStructType.equals("1")){
                 Stack<String> myStack = new Stack<>();
-                this.myList[i] = myStack;
+                myStack.add(initst.nextToken());
+                this.stackList[i] = myStack;
             }
         }
     }
@@ -38,21 +57,18 @@ class QueueStack{
 public class Main {
     static public void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
 
         //큐스택 생성
         QueueStack myQueueStack = new QueueStack(N);
-        myQueueStack.dataStructSettings(br.readLine());
+        myQueueStack.dataStructSettings(br.readLine(),br.readLine());
         
         //큐스택 초기 값 넣기
-        String initData =br.readLine();
+
+        int M = Integer.parseInt(br.readLine());
+        String answer = myQueueStack.inputData(br.readLine(), M);
 
 
-
-
-
-
-        System.out.println(sb);
+        System.out.println(answer);
     }
 }
